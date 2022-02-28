dc24a1649dad9e350b9d5efcb056fe6031f7bdcc
Ala Raddaoui <raddaouiala@gmail.com>
2022-02-28 14:00:08 -0800

    add caching and placeholder pods

--- k8s/remove_nginx_controller.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-set -x
-
-kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-kubectl delete -f ~/nginx_controller_service.yaml
-kubectl delete ns ingress-nginx --ignore-not-found
-rm -f ~/nginx_controller_service.yaml
-kubectl delete ns ingress-nginx
